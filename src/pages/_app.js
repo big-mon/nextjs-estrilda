da0c3bf7cfd48a51368028ecd0f8245d2d3cf11b
BIG-MON <mrmon93@gmail.com>
2021-08-22 06:37:02 +0900

    chore: add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>{process.env.NEXT_PUBLIC_SITE_NAME}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
