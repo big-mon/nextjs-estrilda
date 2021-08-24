3b74a568ace892eca9a39457cb5a6e9292777ba7
BIG-MON <mrmon93+github@gmail.com>
2021-08-24 11:38:20 +0900

    chore: change class

--- src/components/organisms/PostBody.js
+++ src/components/organisms/PostBody.js
@@ -2,5 +2,9 @@ import { htmlToReact } from "../../lib/htmlToReact";
 
 /** 記事本文 */
 export function PostBody({ content }) {
-  return <>{htmlToReact(content)}</>;
+  return (
+    <div className="mx-auto mt-14 mb-8 py-2 max-w-2xl text-xl">
+      {htmlToReact(content)}
+    </div>
+  );
 }
--- src/components/organisms/PostHeader.js
+++ src/components/organisms/PostHeader.js
@@ -4,7 +4,7 @@ export function PostHeader({ title, category, date }) {
   return (
     <div className="mx-auto py-2 max-w-2xl">
       <div className="font-medium text-gray-600 mb-5">{category}</div>
-      <h1 className="text-3xl font-semibold mb-8">{title}</h1>
+      <h1 className="text-4xl font-semibold mb-8">{title}</h1>
       <div className="opacity-70 font-light mb-6">
         <Date dateString={date} />
       </div>
