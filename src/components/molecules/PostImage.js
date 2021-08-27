5928da35497d13978b7a499dd5520a76f054e58d
BIG-MON <mrmon93+github@gmail.com>
2021-08-28 08:13:44 +0900

    fix: image's alt, title

--- src/components/molecules/PostImage.js
+++ src/components/molecules/PostImage.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
 
 /** 投稿画像をImage化 */
-export function PostImage({ title, src }) {
+export function PostImage({ title, src, alt }) {
   const slimSrc = src.replace("/t_postimage,f_auto/", "/");
 
   return (
-    <div className="relative max-h-hero h-screen my-8">
-      <Image src={slimSrc} alt={title} layout="fill" objectFit="contain" />
-    </div>
+    <figure className="my-8">
+      <div className="relative max-h-hero h-screen">
+        <Image src={slimSrc} alt={alt} layout="fill" objectFit="contain" />
+      </div>
+      {title ? (
+        <figcaption className="max-w-2xl mx-auto p-2 text-center text-gray-600 text-base">
+          {title}
+        </figcaption>
+      ) : (
+        <></>
+      )}
+    </figure>
   );
 }
