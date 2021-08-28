f4716f91be9ad975f3b668ef76bf78b3fdad6408
BIG-MON <mrmon93+github@gmail.com>
2021-08-28 10:16:35 +0900

    refactor: remove public, add component

--- src/components/molecules/Logo.js
+++ src/components/molecules/Logo.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import logoPic from "../../../public/icon.svg";
-import { SITE_NAME } from "../../lib/constants";
+import { IconLogo } from "../atoms/IconLogo";
 
 export function Logo() {
   return (
     <>
-      <Image src={logoPic} alt={SITE_NAME} width={25} height={25} />
+      <IconLogo />
     </>
   );
 }
