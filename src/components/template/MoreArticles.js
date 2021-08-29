1a7e72c976a36b97a52371d928c6d6b77790539d
BIG-MON <mrmon93+github@gmail.com>
2021-08-29 17:12:44 +0900

    chore: add group header(tag, category)

--- src/components/template/MoreArticles.js
+++ src/components/template/MoreArticles.js
@@ -1,9 +1,25 @@
 import { PostPreview } from "../organisms/PostPreview";
 import { Pagination } from "../organisms/Pagination";
+import { ArticlesGroup } from "../organisms/ArticlesGroup";
 
-export function MoreArticles({ posts, totalPosts, currentPage, category }) {
+export function MoreArticles({
+  posts,
+  totalPosts,
+  currentPage,
+  category,
+  tags,
+}) {
+  let groupName = "";
+  if (category) {
+    groupName = category;
+  } else if (tags) {
+    groupName = tags;
+  }
+
   return (
     <>
+      <ArticlesGroup>{groupName}</ArticlesGroup>
+
       <div className="max-w-6xl mx-auto my-8 flex flex-wrap gap-y-8">
         {posts.map((post) => (
           <PostPreview
