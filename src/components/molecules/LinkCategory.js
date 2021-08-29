c4b3f8f798d5c940ff9048e28ac1a2b8c52eec32
BIG-MON <mrmon93+github@gmail.com>
2021-08-29 19:49:06 +0900

    fix: common misstake

--- src/components/molecules/LinkCategory.js
+++ src/components/molecules/LinkCategory.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function LinkCategory({ name }) {
   return (
-    <Link href={`/categories/${name}/1`}>
+    <Link href={`/categories/${name.toLowerCase()}/1`}>
       <a className="hover:text-blue-800">{name}</a>
     </Link>
   );
