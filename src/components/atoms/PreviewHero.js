36d5183c7b15dd6a598d8b5d62e929ec84430618
BIG-MON <mrmon93+github@gmail.com>
2021-08-29 09:18:12 +0900

    fix: non src image

--- src/components/atoms/PreviewHero.js
+++ src/components/atoms/PreviewHero.js
@@ -2,13 +2,19 @@ import Image from "next/image";
 
 export function PreviewHero({ title, src, width, height }) {
   return (
-    <Image
-      src={src}
-      alt={title}
-      className="filter group-hover:brightness-50"
-      objectFit="cover"
-      width={width}
-      height={height}
-    />
+    <>
+      {src ? (
+        <Image
+          src={src}
+          alt={title}
+          className="filter group-hover:brightness-50"
+          objectFit="cover"
+          width={width}
+          height={height}
+        />
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
--- src/components/molecules/HeroImage.js
+++ src/components/molecules/HeroImage.js
@@ -2,7 +2,19 @@ import Image from "next/image";
 
 export function HeroImage({ title, src }) {
   const image = (
-    <Image src={src} alt={title} width={1024} height={424} objectFit="cover" />
+    <>
+      {src ? (
+        <Image
+          src={src}
+          alt={title}
+          width={1024}
+          height={424}
+          objectFit="cover"
+        />
+      ) : (
+        <></>
+      )}
+    </>
   );
   return <div className="max-h-hero mx-auto">{image}</div>;
 }
