7c27c888f98c7d38939490c7612dd1cdf9bde3f0
BIG-MON <mrmon93+github@gmail.com>
2021-08-30 09:31:31 +0900

    fix: responsive image margin

--- src/components/molecules/PostImage.js
+++ src/components/molecules/PostImage.js
@@ -5,10 +5,14 @@ export function PostImage({ title, src, alt }) {
   const slimSrc = src.replace("/t_postimage,f_auto/", "/");
 
   return (
-    <figure className="my-8">
-      <div className="relative max-h-hero h-screen">
-        <Image src={slimSrc} alt={alt} layout="fill" objectFit="contain" />
-      </div>
+    <figure className="my-8 relative">
+      <Image
+        src={slimSrc}
+        alt={alt}
+        width={900}
+        height={556}
+        objectFit="contain"
+      />
       {title ? (
         <figcaption className="max-w-2xl mx-auto p-2 text-center text-gray-600 text-base">
           {title}
