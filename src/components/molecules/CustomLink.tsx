5f1fb6c1f764c092240dcbe660af3a8e49fb5267
BIG-MON <mrmon93+github@gmail.com>
2021-09-01 23:09:03 +0900

    fix: link text bug

--- src/components/molecules/CustomLink.tsx
+++ src/components/molecules/CustomLink.tsx
@@ -2,24 +2,29 @@ import Link from "next/link";
 
 type Props = {
   href: string;
+  children: React.ReactNode;
 };
 
 /** 内部/外部リンクの種類に応じてコンポーネントを読み替え */
-export const CustomLink = ({ href }: Props) => {
+export const CustomLink = ({ href, children }: Props) => {
   const isInternal = href && href.startsWith("/");
   const isAnchor = href && href.startsWith("#");
 
   if (isInternal) {
     return (
       <Link href={href}>
-        <a />
+        <a>{children}</a>
       </Link>
     );
   }
 
   if (isAnchor) {
-    return <a href={href} />;
+    return <a href={href}>{children}</a>;
   }
 
-  return <a href={href} target="_blank" rel="noopener noreferrer" />;
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  );
 };
